ea0e207d035624866a2e3a76bb1e89f61d7ebdd6
Mylow <migateakyt@gmail.com>
2021-10-06 20:14:34 +0200

    scrapXML + Update username/password (path)

--- api/mailtohtml.py
+++ api/mailtohtml.py
@@ -1,5 +1,8 @@
 
 import requests
+import os
+username = str(os.environ.get("mailaurion"))
+password = str(os.environ.get("passaurion"))
 
 urll = 'https://aurion.junia.com/faces/Login.xhtml/login'
 ulr = 'https://aurion.junia.com/login'
@@ -22,5 +25,5 @@ rep2 = requests.post(url, cookies=cookies)
 
 # print(rep1.text)
 # print(rep1.cookies)
-print(rep2.text)
+# print(rep2.text)
 print(rep2.cookies)
--- api/testuser.py
+++ api/testuser.py
@@ -1,14 +1,20 @@
 
 import requests
+import os
+username = str(os.environ.get("mailaurion"))
+password = str(os.environ.get("passaurion"))
 
 urll = 'https://aurion.junia.com/faces/Login.xhtml'
 url = 'https://aurion.junia.com/faces/Planning.xhtml'
 log = 'https://aurion.junia.com/login'
 
-data = {'username': username,'password': password}
+data = str('username=' + username + '&password=' + password)
+print(data)
 
 # https://stackoverflow.com/questions/26615756/python-requests-module-sends-json-string-instead-of-x-www-form-urlencoded-param
 
+
+##methode POST
 rep1 = requests.post(urll, data=data)
 rep2 = requests.post(log, data=data)
 rep3 = requests.post(url, data=data)
@@ -19,24 +25,24 @@ print(rep2.headers)
 # print(rep3.text)
 print(rep3.headers)
 
+##methode GET
+# rep1 = requests.get(urll, data=data)
+# rep2 = requests.get(log, data=data)
+# rep3 = requests.get(url, data=data)
+# # print(rep1.text)
+# print(rep1.headers)
+# # print(rep2.text)
+# print(rep2.headers)
+# # print(rep3.text)
+# print(rep3.headers)
 
-rep1 = requests.get(urll, data=data)
-rep2 = requests.get(log, data=data)
-rep3 = requests.get(url, data=data)
-# print(rep1.text)
-print(rep1.headers)
-# print(rep2.text)
-print(rep2.headers)
-# print(rep3.text)
-print(rep3.headers)
-
-
-rep1 = requests.put(urll, data=data)
-rep2 = requests.put(log, data=data)
-rep3 = requests.put(url, data=data)
-# print(rep1.text)
-print(rep1.headers)
-# print(rep2.text)
-print(rep2.headers)
-# print(rep3.text)
-print(rep3.headers)
+##methode PUT
+# rep1 = requests.put(urll, data=data)
+# rep2 = requests.put(log, data=data)
+# rep3 = requests.put(url, data=data)
+# # print(rep1.text)
+# print(rep1.headers)
+# # print(rep2.text)
+# print(rep2.headers)
+# # print(rep3.text)
+# print(rep3.headers)
