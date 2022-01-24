95b71989e6ecfc82a4ac4d69d7318f94fd34f442
Mylow <migateakyt@gmail.com>
2022-01-24 18:51:10 +0100

    UP calcul maths / physiques

--- NoFileMoyenne/py_app/api/CalcMoyenne.py
+++ NoFileMoyenne/py_app/api/CalcMoyenne.py
@@ -4,16 +4,45 @@ locale.setlocale(locale.LC_ALL, 'fr_FR')
 
 
 def maths(result):
-    maths,partiel,coef = [],[],3
+    maths,partiel,coef,TP,IE,sumIE = [],[],3,[],[],0
+    # print(result)
     for i in range(len(result)):
-        if (("MATH1" in result[i][1]) or ("MATH2" in result[i][1]) or ("MATH3" in result[i][1]) or ("MATH4" in result[i][1])): 
-            if (("P1" not in result[i][1]) and ("P2" not in result[i][1])):
-                if (result[i][1] != result[i-1][1]):
-                    maths.append(result[i])
-            else:
+        if (("MATH" in result[i][1][4])):
+            # print(result[i][1])
+            if (("P1" in result[i][1]) or ("P2" in result[i][1])):
                 if (result[i][1] != result[i-1][1]):
                     partiel.append(result[i])
-    return maths, partiel, coef
+            elif (("TP" in result[i][1][5])):
+                if (result[i][1] != result[i-1][1]):
+                    TP.append(result[i])
+            elif (("IE" in result[i][1][5])):
+                if (result[i][1] != result[i-1][1]):
+                    IE.append(result[i])
+                    sumIE += float(locale.atof(result[i][3]))
+            else:
+                if (result[i][1] != result[i-1][1]):
+                    maths.append(result[i])
+    # print(IE,"\n",partiel,"\n",TP,"\n",maths) 
+
+    IE = (Moyenne(matiere(IE)[0]))
+    maths = matiere(maths)
+    maths[0].append(IE)
+    note = Moyenne(maths[0])
+    # print(note)
+    notep = Moyenne(matiere(partiel)[0])
+    # print(notep)
+
+    notetp = Moyenne(matiere(TP)[0])
+    # print(notetp)
+
+    note = (note*0.4) + (notep*0.6)
+    # print(note)
+    notes = [note,notetp]
+    coefs = [12,4]
+    final = MoyenneC(notes, coefs)
+    # print(final)
+    # Maths coef 12 + TP coef 4
+    return final
 
 def prog(result):
     prog,partiel,ds,tp,coef = [],[],[],[],3
@@ -51,7 +80,30 @@ def web(result):
                     ds.append(result[i])
     return web, partiel, ds, coef
 
-def optique(result):
+def physique(result):
+    elec,partiel,TP,coef = [],[],[],3
+    for i in range(len(result)):
+        if ("ELEC" in result[i][1]) : 
+            if (("P1" in result[i][1]) or ("P2" in result[i][1])):
+                if (result[i][1] != result[i-1][1]):
+                    partiel.append(result[i])
+            elif (("TP" in result[i][1][5])) :
+                if (result[i][1] != result[i-1][1]):
+                    TP.append(result[i])
+            else:
+                if (result[i][1] != result[i-1][1]):
+                    elec.append(result[i])
+                    
+    note = Moyenne(matiere(elec)[0])
+    # print(note)
+    notep = Moyenne(matiere(partiel)[0])
+    # print(notep)
+    TPelec = Moyenne(matiere(TP)[0])
+    # print(notetp)
+    noteelec = (note*0.4) + (notep*0.6)
+    # print(noteelec,TPelec)
+
+
     opt,partiel,coef = [],[],2
     for i in range(len(result)):
         if ("OPTIQUE" in result[i][1]) : 
@@ -61,21 +113,20 @@ def optique(result):
             else:
                 if (result[i][1] != result[i-1][1]):
                     partiel.append(result[i])
-    return opt, partiel, coef
-def electronique(result):
-    elec,partiel,tp,coef = [],[],[],3
-    for i in range(len(result)):
-        if ("ELEC" in result[i][1]) : 
-            if (("P1" not in result[i][1]) and ("P2" not in result[i][1])):
-                if (result[i][1] != result[i-1][1]):
-                    elec.append(result[i])
-            if (("TP1" in result[i][1]) or ("TP2" in result[i][1]) or ("TP3" in result[i][1]) or ("TP4" in result[i][1]) or ("TP5" in result[i][1]) or ("TP6" in result[i][1]) or ("TP7" in result[i][1]) or ("TP8" in result[i][1]) or ("TP9" in result[i][1])):
-                if (result[i][1] != result[i-1][1]):
-                    tp.append(result[i])
-            else:
-                if (result[i][1] != result[i-1][1]):
-                    partiel.append(result[i])
-    return elec, partiel, coef
+                    
+
+    note = Moyenne(matiere(opt)[0])
+    notep = Moyenne(matiere(partiel)[0])
+    # print(note,notep)
+    noteopt = (note*0.4) + (notep*0.6)
+    # print(noteopt)
+
+    note = [noteopt, noteelec, TPelec]
+    coef = [2,3,3]
+    final = MoyenneC(note, coef)
+    return(final)
+
+
 def mecanique(result):
     meca,partiel,coef = [],[],3
     for i in range(len(result)):
@@ -121,7 +172,7 @@ def matiere(result):
     return Notes,Coefs
     
 # moyenne a partir d'une liste de note du module
-def Moyenne(Notes, Coefs):
+def Moyenne(Notes):
     return sum(Notes)/len(Notes)
 
 def MoyenneC(Notes, Coefs):
@@ -129,13 +180,8 @@ def MoyenneC(Notes, Coefs):
 
 
 def MATHS(resultats):
-    # print(matiere(maths(resultats)[0]))
-    # print(matiere(maths(resultats)[1]))
-    note = (Moyenne(matiere(maths(resultats)[0])[0],matiere(maths(resultats)[0])[1]))
-    notep = (Moyenne(matiere(maths(resultats)[1])[0],matiere(maths(resultats)[1])[1]))
-
-    note = (note*0.4) + (notep*0.6)
-    return(round(note,2))
+    final = maths(resultats)
+    return(round(final,2))
 # MATHS()
 
 
@@ -143,9 +189,9 @@ def PROG(resultats):
     # print(matiere(prog(resultats)[0]))
     # print(matiere(prog(resultats)[1]))
     # print(matiere(prog(resultats)[2]))
-    note = (Moyenne(matiere(prog(resultats)[0])[0],matiere(prog(resultats)[0])[1]))
-    notep = (Moyenne(matiere(prog(resultats)[1])[0],matiere(prog(resultats)[1])[1]))
-    noteds = (Moyenne(matiere(prog(resultats)[2])[0],matiere(prog(resultats)[2])[1]))
+    note = (Moyenne(matiere(prog(resultats)[0])[0]))
+    notep = (Moyenne(matiere(prog(resultats)[1])[0]))
+    noteds = (Moyenne(matiere(prog(resultats)[2])[0]))
     
     
     if (notep == 0):
@@ -162,9 +208,9 @@ def WEB(resultats):
     # print(matiere(web(resultats)[0]))
     # print(matiere(web(resultats)[1]))
     # print(matiere(web(resultats)[2]))
-    note = (Moyenne(matiere(web(resultats)[0])[0],matiere(web(resultats)[0])[1]))
-    notep = (Moyenne(matiere(web(resultats)[1])[0],matiere(web(resultats)[1])[1]))
-    noteds = (Moyenne(matiere(web(resultats)[2])[0],matiere(web(resultats)[2])[1]))
+    note = (Moyenne(matiere(web(resultats)[0])[0]))
+    notep = (Moyenne(matiere(web(resultats)[1])[0]))
+    noteds = (Moyenne(matiere(web(resultats)[2])[0]))
     
     
     if (notep == 0):
@@ -191,27 +237,14 @@ def INFO(resultats):
 
 
 def PHYSIQUE(resultats):
-    note = (Moyenne(matiere(optique(resultats)[0])[0],matiere(optique(resultats)[0])[1]))
-    notep = (Moyenne(matiere(optique(resultats)[1])[0],matiere(optique(resultats)[1])[1]))
-    noteopt = (note*0.4) + (notep*0.6)
-    
-    note = (Moyenne(matiere(electronique(resultats)[0])[0],matiere(electronique(resultats)[0])[1]))
-    notep = (Moyenne(matiere(electronique(resultats)[1])[0],matiere(electronique(resultats)[1])[1]))
-    noteelec = (note*0.4) + (notep*0.6)
-    
-    if (noteopt == 0): note = noteelec
-    if (noteelec == 0): note = noteopt
-    else:
-        note = [noteopt, noteelec]
-        coef = [2,3]
-        note = MoyenneC(note, coef)
+    note = physique(resultats)
     return(round(note,2))
 # PHYSIQUE()
 
 def DEV(resultats):
-    noteang = (Moyenne(matiere(anglais(resultats)[0])[0],matiere(anglais(resultats)[0])[1]))
-    notecomm = (Moyenne(matiere(comm(resultats)[0])[0],matiere(comm(resultats)[0])[1]))
-    notespo = (Moyenne(matiere(sport(resultats)[0])[0],matiere(sport(resultats)[0])[1]))
+    noteang = (Moyenne(matiere(anglais(resultats)[0])[0]))
+    notecomm = (Moyenne(matiere(comm(resultats)[0])[0]))
+    notespo = (Moyenne(matiere(sport(resultats)[0])[0]))
     note = [noteang, notecomm, notespo]
     coef = [2,2,2]
     note = MoyenneC(note, coef)
@@ -219,8 +252,9 @@ def DEV(resultats):
 # DEV()
 
 
-def main(username,password,stocknote):
+def main(username,password):
     result = getnotes.main(username,password)
+    # print(result)
     resultats = TableNotes.table(result)  #Liste de liste de note sous forme ['07/01/2022', ['2122', 'ISEN', 'CIR1', 'S1', 'WEB', 'P1'], 'Partiel de Technologies Web', ' 16.60', '24']
-        
+    # print(resultats)
     return MATHS(resultats),PHYSIQUE(resultats),INFO(resultats),DEV(resultats)
---
+++ TestZone (moyenne)/main.py
@@ -0,0 +1,69 @@
+result = [['07/01/2022', ['2122', 'ISEN', 'CIR1', 'S1', 'WEB', 'P1'], 'Partiel de Technologies Web', ' 16.60', '24'], ['06/01/2022', ['2122', 'ISEN', 'CIR1', 'S1', 'PROG1', 'P1'], 'Epreuve de Partiel', ' 16.20', '40'], ['05/01/2022', ['2122', 'ISEN', 'CIR1', 'S1', 'ELEC', 'P1'], 'Epreuve de Partiel', '  4.00', '60'], ['03/01/2022', ['2122', 'ISEN', 'CIR1', 'S1', 'MATH2', 'P1'], 'Partiel de Mathématiques', '  4.00', '1'], ['10/12/2021', ['2122', 'ISEN', 'CIR1', 'S1', 'MATH2', 'CC5'], 'Contrôle Continu de Mathématiques n°5', '  6.40', '1'], ['10/12/2021', ['2122', 'ISEN', 'CIR1', 'S1', 'WEB', 'CC2'], 'Contrôle Continu de Technologies Web n°2', ' 12.67', '12'], ['09/12/2021', ['2122', 'ISEN', 'CIR1', 'S1', 'ELEC', 'DS'], "Devoir Surveillé d'Électronique", '  7.00', '1'], ['08/12/2021', ['2122', 'ISEN', 'CIR1', 'S1', 'ELEC', 'TP4', '1'], 'Evaluation du TP4', ' 19.00', '1'], ['06/12/2021', ['2122', 'ISEN', 'CIR1', 'S1', 'ANGLAIS', '1', 'NOTE'], "Moyenne d'Anglais (groupe 1)", ' 14.04', '1'], ['06/12/2021', ['2122', 'ISEN', 'CIR1', 'S1', 'SPORT', 'T1'], 'Note de Sport Tiers 1', ' 15.00', '1'], ['06/12/2021', ['2122', 'ISEN', 'CIR1', 'S1', 'WEB', 'PROJ'], 'Projet de Technologies Web', ' 15.00', '20'], ['26/11/2021', ['2122', 'ISEN', 'CIR1', 'S1', 'MATH2', 'CC4'], 'Contrôle Continu de Mathématiques n°4', '  7.10', '1'], ['26/11/2021', ['2122', 'ISEN', 'CIR1', 'S1', 'PROG1', 'CC2'], 'Contrôle Continu n°2', ' 13.75', '1'], ['24/11/2021', ['2122', 'ISEN', 'CIR1', 'S1', 'MATH2', 'IE3'], 'Interrogation de cours n°3', '  0.00', '0,33'], ['24/11/2021', ['2122', 'ISEN', 'CIR1', 'S1', 'MATH2', 'IE3'], 'Interrogation de cours n°3', '  0.00', '1'], ['24/11/2021', ['2122', 'ISEN', 'CIR1', 'S1', 'ELEC', 'TP3', '1'], 'Evaluation du TP3', ' 14.00', '1'], ['19/11/2021', ['2122', 'ISEN', 'CIR1', 'S1', 'WEB', 'DS'], 'DS Technologies Web', ' 15.12', '12'], ['17/11/2021', ['2122', 'ISEN', 'CIR1', 'S1', 'MATH2', 'IE2'], 'Interrogation de cours n°2', '  8.00', '0,33'], ['17/11/2021', ['2122', 'ISEN', 'CIR1', 'S1', 'MATH2', 'IE2'], 'Interrogation de cours n°2', '  8.00', '1'], ['17/11/2021', ['2122', 'ISEN', 'CIR1', 'S1', 'MATH', 'TP4', '1'], 'Evaluation du TP4', ' 17.00', '1'], ['13/11/2021', ['2122', 'ISEN', 'CIR1', 'S1', 'MATH1', 'P2'], 'Examen de Mathématiques (seconde session)', ' 11.25', '1'], ['12/11/2021', ['2122', 'ISEN', 'CIR1', 'S1', 'ELEC', 'CC'], "Contrôle Continu d'Electronique", ' 11.50', '1'], ['12/11/2021', ['2122', 'ISEN', 'CIR1', 'S1', 'MATH2', 'CC3'], 'Contrôle Continu de Mathématiques n°3', ' 12.00', '1'], ['10/11/2021', ['2122', 'ISEN', 'CIR1', 'S1', 'MATH2', 'IE1'], 'Interrogation de cours n°1', ' 15.00', '0,33'], ['10/11/2021', ['2122', 'ISEN', 'CIR1', 'S1', 'MATH2', 'IE1'], 'Interrogation de cours n°1', ' 15.00', '1'], ['10/11/2021', ['2122', 'ISEN', 'CIR1', 'S1', 'ELEC', 'TP2', '1'], 'Evaluation du TP2', ' 10.00', '1'], ['22/10/2021', ['2122', 'ISEN', 'CIR1', 'S1', 'PROG1', 'DS'], 'Devoir surveillé de Programmation', ' 17.20', '20'], ['20/10/2021', ['2122', 'ISEN', 'CIR1', 'S1', 'MATH', 'TP3', '1'], 'Evaluation du TP3', ' 17.00', '1'], ['15/10/2021', ['2122', 'ISEN', 'CIR1', 'S1', 'MATH1', 'P1'], 'Partiel de Mathématiques', '  3.50', '1'], ['15/10/2021', ['2122', 'ISEN', 'CIR1', 'S1', 'MATH1', 'P1'], 'Partiel de Mathématiques', '  3.50', '2'], ['13/10/2021', ['2122', 'ISEN', 'CIR1', 'S1', 'ELEC', 'TP1', '1'], 'Evaluation du TP1', ' 13.00', '1'], ['08/10/2021', ['2122', 'ISEN', 'CIR1', 'S1', 'OPTIQUE', 'P1'], "Partiel d'Optique Géométrique", '  9.00', '1'], ['04/10/2021', ['2122', 'ISEN', 'CIR1', 'S1', 'COMM', 'ORAL'], 'Exposé oral', ' 17.00', '1'], ['01/10/2021', ['2122', 'ISEN', 'CIR1', 'S1', 'MATH1', 'CC2'], 'Contrôle Continu de Mathématiques n°2', ' 11.00', '1'], ['01/10/2021', ['2122', 'ISEN', 'CIR1', 'S1', 'WEB', 'CC'], 'Contrôle Continu de Technologies Web n°1', ' 13.94', '12'], ['24/09/2021', ['2122', 'ISEN', 'CIR1', 'S1', 'PROG1', 'CC1'], 'Contrôle Continu de Programmation', ' 11.67', '1'], ['17/09/2021', ['2122', 'ISEN', 'CIR1', 'S1', 'MATH1', 'CC1'], 'Contrôle Continu de Mathématiques n°1', '  9.80', '1'], ['17/09/2021', ['2122', 'ISEN', 'CIR1', 'S1', 'OPTIQUE', 'CC'], "Contrôle Continu d'Optique Géométrique", ' 13.00', '1'], ['10/09/2021', ['2122', 'ISEN', 'CIR1', 'S1', 'MATH1', 'CC0'], 'QCM mathématiques de rentrée', ' 14.50']]
+import locale
+locale.setlocale(locale.LC_ALL, 'fr_FR')
+# Calcul par rapport au coef des notes de result
+def matiere(result):
+    Notes = []
+    Coefs = []
+    for i in range (len(result)):
+        # print(result[i])
+        if (len(result[i]) == 5):
+            Notes.append(float(locale.atof(result[i][3])))
+            Coefs.append(float(locale.atof(result[i][4])))
+    return Notes,Coefs
+# moyenne a partir d'une liste de note du module
+def Moyenne(Notes):
+    return sum(Notes)/len(Notes)
+def MoyenneC(Notes, Coefs):
+    return sum(Notes[g] * Coefs[g] / sum(Coefs) for g in range(len(Notes)))
+
+
+elec,partiel,TP,coef = [],[],[],3
+for i in range(len(result)):
+    if ("ELEC" in result[i][1]) : 
+        if (("P1" in result[i][1]) or ("P2" in result[i][1])):
+            if (result[i][1] != result[i-1][1]):
+                partiel.append(result[i])
+        elif (("TP" in result[i][1][5])) :
+            if (result[i][1] != result[i-1][1]):
+                TP.append(result[i])
+        else:
+            if (result[i][1] != result[i-1][1]):
+                elec.append(result[i])
+                
+note = Moyenne(matiere(elec)[0])
+# print(note)
+notep = Moyenne(matiere(partiel)[0])
+# print(notep)
+
+TPelec = Moyenne(matiere(TP)[0])
+# print(notetp)
+
+noteelec = (note*0.4) + (notep*0.6)
+# print(noteelec,TPelec)
+
+
+
+
+opt,partiel,coef = [],[],2
+for i in range(len(result)):
+    if ("OPTIQUE" in result[i][1]) : 
+        if (("P1" not in result[i][1]) and ("P2" not in result[i][1])):
+            if (result[i][1] != result[i-1][1]):
+                opt.append(result[i])
+        else:
+            if (result[i][1] != result[i-1][1]):
+                partiel.append(result[i])
+                
+
+note = Moyenne(matiere(opt)[0])
+notep = Moyenne(matiere(partiel)[0])
+# print(note,notep)
+noteopt = (note*0.4) + (notep*0.6)
+# print(noteopt)
+
+note = [noteopt, noteelec, TPelec]
+coef = [2,3,3]
+final = MoyenneC(note, coef)
+
+print(final)
